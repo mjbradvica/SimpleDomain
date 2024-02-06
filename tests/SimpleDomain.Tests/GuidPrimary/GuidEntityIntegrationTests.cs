d1c21f34284a23b175902806f5f66eade22c4fd3
Michael Bradvica <mjbradvica@gmail.com>
2024-02-05 19:08:12 -0500

    test fix

--- tests/SimpleDomain.Tests/GuidPrimary/GuidEntityIntegrationTests.cs
+++ tests/SimpleDomain.Tests/GuidPrimary/GuidEntityIntegrationTests.cs
@@ -100,7 +100,7 @@ namespace SimpleDomain.Tests.GuidPrimary
             {
                 await connection.OpenAsync();
 
-                var result = await connection.QueryFirstAsync<TestGuidEntity>($"SELECT * FROM dbo.GuidEntities WHERE ID='{id}'");
+                var result = await connection.QueryFirstAsync<TestGuidEntity>($"SELECT * FROM dbo.GuidEntities WHERE Id='{id}'");
 
                 await connection.CloseAsync();
 
