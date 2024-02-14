3fbcc24c0a26aac3b6eb16849bd7f5ece0e2d0d5
Michael Bradvica <mjbradvica@gmail.com>
2024-02-14 08:02:53 -0500

    update tests

---
+++ tests/ClearDomain.Tests/LongPrimary/TestLongEntity.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="TestLongEntity.cs" company="Michael Bradvica LLC">
+// Copyright (c) Michael Bradvica LLC. All rights reserved.
+// </copyright>
+
+using ClearDomain.LongPrimary;
+
+namespace ClearDomain.Tests.LongPrimary
+{
+    /// <summary>
+    /// Test long entity.
+    /// </summary>
+    public class TestLongEntity : Entity
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestLongEntity"/> class.
+        /// </summary>
+        public TestLongEntity()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestLongEntity"/> class.
+        /// </summary>
+        /// <param name="id">The identifier for the test entity.</param>
+        public TestLongEntity(long id)
+            : base(id)
+        {
+        }
+    }
+}
