354e140beeaa3248ea7fb799a478e4bf9ec1b049
Michael Bradvica <mjbradvica@gmail.com>
2024-02-14 07:45:14 -0500

    fix build

---
+++ tests/ClearDomain.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+// <copyright file="UnitTest1.cs" company="Michael Bradvica LLC">
+// Copyright (c) Michael Bradvica LLC. All rights reserved.
+// </copyright>
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClearDomain.Tests
+{
+    /// <summary>
+    /// Test Class.
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// TestMethod.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
